b9420f06066974133141c44267092754ddefc8fc
Phil Mauracher <gradientus@gmail.com>
2020-08-07 09:21:41 -0600

    Added inrange functionality.  Next implementation is words.

--- _.js
+++ _.js
@@ -3,7 +3,25 @@ const _ = {
     number = Math.min(number, upper);
     number = Math.max(number, lower);
     return number;
+  },
+
+  inRange(num, start, end) {
+    if (!end) {
+      end = start;
+      start = 0;
+    } else if (start > end) {
+      let tempVal = end;
+      end = start;
+      start = tempVal;
+    }
+
+    if (num >= start && num < end) {
+      return true;
+    } else {
+      return false;
+    }
   }
+
 };
 
 
--- testing.js
+++ testing.js
@@ -3,3 +3,30 @@ const clamp = (num1, num2, num3) => {
   num1 = Math.max(num1, num2);
   return num1;
 };
+
+/*
+params: num, start, end
+
+now check these:
+if num is smaller than start return false
+if num is larger than end return false
+if num between smaller and larger return true
+
+*/
+
+const inRange = (num, start, end) => {
+  if (!end) {
+    end = start;
+    start = 0;
+  } else if (start > end) {
+    let tempVal = end;
+    end = start;
+    start = tempVal;
+  }
+
+  if (num > start && num < end) {
+    return true;
+  } else {
+    return false;
+  }
+};
